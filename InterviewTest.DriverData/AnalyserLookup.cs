fbe53c2e4c616f27c9f5100f267c28e68dbae109
Tushar Narale <tushar.narale@northgateps.com>
2018-03-22 12:48:12 +0530

    Task 2 : Refactored Analyser Lookup by removing switch cases and instead using Dictionary that returns delegates

--- InterviewTest.DriverData/AnalyserLookup.cs
+++ InterviewTest.DriverData/AnalyserLookup.cs
@@ -1,26 +1,75 @@
-using System;
 using InterviewTest.DriverData.Analysers;
 using InterviewTest.DriverData.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace InterviewTest.DriverData
 {
-	public static class AnalyserLookup
-	{
-		public static IAnalyser GetAnalyser(string type)
-		{
-			switch (type)
-			{
-				case "friendly":
-					return new FriendlyAnalyser();
-                case "delivery":
-                    return new DeliveryDriverAnalyser(new DriverConfiguration() { StartTime = new TimeSpan(9,0,0), EndTime = new TimeSpan(17,0,0), MaxSpeed = 30m, RatingForExceedingSpeedLimit = 0.0m, IsPenaltyApplicable = true, PenaltyForUndocumentedPeriod = 0.5m });
-                case "formulaone":
-                    return new FormulaOneAnalyser(new DriverConfiguration() { StartTime = TimeSpan.Zero, EndTime = TimeSpan.Zero, MaxSpeed = 200m, RatingForExceedingSpeedLimit = 1.0m, IsPenaltyApplicable = true, PenaltyForUndocumentedPeriod = 0.5m });
-                case "getaway":
-                    return new GetawayDriverAnalyser(new DriverConfiguration() { StartTime = new TimeSpan(13, 0, 0), EndTime = new TimeSpan(14, 0, 0), MaxSpeed = 80m, RatingForExceedingSpeedLimit = 1.0m, IsPenaltyApplicable = true, PenaltyForUndocumentedPeriod = 0.5m });
-                default:
-					throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
-			}
-		}
-	}
+    public static class AnalyserLookup
+    {
+        public static IAnalyser GetAnalyser(string type)
+        {
+            if (analyserList.ContainsKey(type))
+                return analyserList[type]();
+
+            throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
+        }
+        
+        public static Dictionary<string, Func<IAnalyser>> analyserList =
+                    new Dictionary<string, Func<IAnalyser>>
+                {
+                    { "delivery", DeliveryDriverAnalyser },
+                    { "friendly", FriendlyAnalyser },
+                    { "formulaone", FormulaOneAnalyser },
+                    { "getaway", GetawayDriverAnalyser }
+                };
+
+        private static DeliveryDriverAnalyser DeliveryDriverAnalyser()
+        {
+            var driverConfiguration = new DriverConfiguration() {
+                StartTime = new TimeSpan(9, 0, 0),
+                EndTime = new TimeSpan(17, 0, 0),
+                MaxSpeed = 30m,
+                RatingForExceedingSpeedLimit = 0.0m,
+                IsPenaltyApplicable = true,
+                PenaltyForUndocumentedPeriod = 0.5m
+            };
+            return new DeliveryDriverAnalyser(driverConfiguration);
+        }
+
+        private static FriendlyAnalyser FriendlyAnalyser()
+        {
+            return new FriendlyAnalyser();
+        }
+
+        private static FormulaOneAnalyser FormulaOneAnalyser()
+        {
+            var driverConfiguration = new DriverConfiguration()
+            {
+                StartTime = TimeSpan.Zero,
+                EndTime = TimeSpan.Zero,
+                MaxSpeed = 200m,
+                RatingForExceedingSpeedLimit = 1.0m,
+                IsPenaltyApplicable = true,
+                PenaltyForUndocumentedPeriod = 0.5m
+            };
+
+            return new FormulaOneAnalyser(driverConfiguration);
+        }
+
+        private static GetawayDriverAnalyser GetawayDriverAnalyser()
+        {
+            var driverConfiguration = new DriverConfiguration()
+            {
+                StartTime = new TimeSpan(13, 0, 0),
+                EndTime = new TimeSpan(14, 0, 0),
+                MaxSpeed = 80m,
+                RatingForExceedingSpeedLimit = 1.0m,
+                IsPenaltyApplicable = true,
+                PenaltyForUndocumentedPeriod = 0.5m
+            };
+
+            return new GetawayDriverAnalyser(driverConfiguration);
+        }
+    }
 }
